35957e38315dc0563e3145f07054672639cba24f
ivan.aramazov <ivan.aramazov@gmail.com>
2018-12-11 15:49:53 +0200

    Final Commit Before attempting Location FK

--- ScheduleCalendar-V2/Controllers/LocationsController.cs
+++ ScheduleCalendar-V2/Controllers/LocationsController.cs
@@ -20,6 +20,15 @@ namespace ScheduleCalendar_V2.Controllers
             return View(db.Locations.ToList());
         }
 
+        public JsonResult GetLocationsDropdown()
+        {
+            using (ScheduleCalendarContext_V2 cntx = new ScheduleCalendarContext_V2())
+            {
+                var events = cntx.Locations.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
         // GET: Locations/Details/5
         public ActionResult Details(int? id)
         {
